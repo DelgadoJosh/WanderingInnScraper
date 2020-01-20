26732be179bd2dbd4c5cce69f15e7d34394f9f13
DelgadoJosh <34148647+DelgadoJosh@users.noreply.github.com>
2020-01-19 21:20:15 -0500

    Initial Commit to Github

---
+++ Scripts/wanderingInnScraperBackEnd.py
@@ -0,0 +1,149 @@
+import os
+import csv
+import requests
+from bs4 import BeautifulSoup
+from bs4 import NavigableString
+
+# GLOBALS
+curPageNum = 0
+word_count = 0
+debug = False
+if debug:
+  meta_file = open("00000 META.txt", "wb")
+print_option = ''
+headers = {
+  # This header is used to mark the webscraper so the server knows
+  # who's currently scraping it.
+  'User-Agent': 'An interested fan',
+  'From': 'fak3unknown1@gmail.com'
+}
+
+# Function that handles writing to a file.
+def writeToFile(fileToWrite, stringToWrite):
+  global meta_file
+  global print_option
+  
+  if print_option == 'One Large File':
+    meta_file.write(stringToWrite.encode('utf8'))
+  elif print_option == 'Individual Chapters':
+    fileToWrite.write(stringToWrite.encode('utf8'))
+  elif print_option == 'Both':
+    meta_file.write(stringToWrite.encode('utf8'))
+    fileToWrite.write(stringToWrite.encode('utf8'))
+
+# Function to initialize scraping the page.
+def scrapePageInit(start_page_url, stop_page_url, local_print_option, directory):
+  global print_option 
+  global meta_file 
+  print_option = local_print_option
+  meta_file = open(directory + "/The Wandering Inn.txt", "wb")
+
+  scrapePage(start_page_url, stop_page_url, directory)
+
+# Recursive function to scrape the page using Python BeautifulSoup
+def scrapePage(url, stop_page_url, directory):
+  global curPageNum
+  global word_count
+  global print_option
+
+  # Removes the .wordpress found on the site
+  url = url.replace(".wordpress","")  
+  if debug:
+    print(f"\nCurrently at {url}.")
+
+  # Appends a '/' at the end if it's not seen in the url
+  # This is to allow the inputted "stop" address to stop if it 
+  # encounters an address that does not end in a '/'
+  if(url[len(url)-1] != '/'):
+    url += '/'
+  
+  if debug:
+    print(f"\nUrl = {url} and stop_page_url = {stop_page_url}")
+
+  # Accesses the page
+  page = requests.get(url, headers)
+
+  # Create a BeautifulSoup Object (aka parse Tree), and parse with built-in html.parser
+  soup = BeautifulSoup(page.text, 'html.parser')
+
+  # Grabs the title from the "entry-title" h1
+  chapter_title_list = soup.find_all('h1', class_='entry-title')
+  title = chapter_title_list[0].contents[0]
+  if debug:
+    print(title)
+  print(f"\nCurrently Scraping {url} - {title}")
+
+  # Creates a file for this specific chapter, only if needed
+  fileTitle = F"{curPageNum:03d} {title}.txt"
+
+  # fileTitleDirectory = f"{os.getcwd()}\\Chapters\\{curPageNum:03d} {title}.txt"
+  fileTitleDirectory = directory + "/" + fileTitle
+  file = meta_file
+  if print_option != 'One Large File':
+    file = open(fileTitleDirectory, "wb")
+
+  writeToFile(file, title)
+  writeToFile(file, "\r\n")
+  writeToFile(file, "\r\n")
+
+
+  # Pull all text from the "entry-content" div
+  chapter_paragraph_list = soup.find(class_='entry-content')
+  
+  # Pull text from all instances of <p> tag within BodyText div
+  chapter_paragraph_list_items = chapter_paragraph_list.find_all('p')
+
+  # Grabs the final paragraph tag (which contains the next chapter)
+  last_paragraph_item = chapter_paragraph_list_items[-1]
+  if debug:
+    print(f'Last item: {last_paragraph_item.contents[0]}')
+  link_list = last_paragraph_item.find_all('a') # Grabs the <a> tags
+  
+  # Grabs the final paragraph that has an a tag
+  for paragraph_item in reversed(chapter_paragraph_list_items):
+    cur_link_list = paragraph_item.find_all('a')
+    if(len(cur_link_list) > 0):
+      link_list = cur_link_list
+      break
+  if(len(link_list) == 0):
+    print("Stopped due to no next_chapter_link found")
+    return
+  next_chapter_link = link_list[-1]  # Grabs the final link to the next one
+  next_chapter_url = next_chapter_link.get('href')
+
+
+  # Create a for loop to print out all paragraph texts (except the last).
+  # https://stackoverflow.com/questions/914715/how-to-loop-through-all-but-the-last-item-of-a-list
+  for chapter_paragraph in chapter_paragraph_list_items[:-1]:
+    
+    # Goes through every tag within the paragraph.
+    for chapter_paragraph_part in chapter_paragraph.contents[:-1]:
+      text = chapter_paragraph_part
+      if(not(isinstance(chapter_paragraph_part, NavigableString))):  
+        text = chapter_paragraph_part.get_text()
+
+      writeToFile(file, text)
+      word_count += len(text.split())
+
+    chapter_paragraph_last_part = chapter_paragraph.contents[-1]
+    text = chapter_paragraph_last_part
+    if(not(isinstance(chapter_paragraph_last_part, NavigableString))):  
+      text = chapter_paragraph_last_part.get_text()
+
+    writeToFile(file, text)
+    writeToFile(file, "\r\n")
+    writeToFile(file, "\r\n")
+    word_count += len(text.split())
+
+
+  print(f"Word Count: {word_count}")
+  curPageNum = curPageNum + 1
+
+  # Break out if done.
+  if(url == stop_page_url):
+    print("Reached the stopping page url, stopping.")
+    return
+
+  # Otherwise go to the next link and continue.
+  scrapePage(next_chapter_url, stop_page_url, directory)
+
---
+++ Scripts/wanderingInnScraperFrontEnd.py
@@ -0,0 +1,123 @@
+# This code creates a very simple GUI to run the Wandering Inn Scraper
+
+import PySimpleGUI as sg 
+import wanderingInnScraperBackEnd as backend
+
+sg.theme('Dark Brown')
+
+
+# --- Helper Functions ---
+def getDebug(values):
+  text = ""
+  text = text + "Type of output file: " + values['print_option'] + "\n"
+  text = text + "Beginning Link: " + values['beginning_link'] + "\n"
+  text = text + "Ending Link: " + values['ending_link'] + "\n"
+  text = text + "Folder Address: " + values['folder_location'] + "\n"
+  return text
+
+def getAboutText():
+  text = ""
+  text = text + "Wandering Inn Scraper\n"
+  text = text + "by Josh Delgdao\n"
+  return text
+
+# Debug Setting
+debug = False
+
+
+# --- Layout ---
+# Defines the Menu Header
+menu_def = [['File', ['Exit']],
+            ['Help', 'About...'],]
+
+print_options = ('One Large File', 'Individual Chapters', 'Both')
+
+default_beginning_link = "https://wanderinginn.com/2016/07/27/1-00/"
+default_ending_link = ""
+
+default_folder = "Please pick a folder!"
+
+# Options
+left_side_width = 50
+left_side_layout = [
+  # Menu
+  [sg.Menu(menu_def, tearoff=True)],
+
+  # Output Option
+  [sg.Text("Please select what type of output file:", size=(left_side_width, 1))],
+  [sg.InputOptionMenu(print_options, key='print_option', size=((int)(left_side_width*0.8), 1))],
+  [sg.Text('_'*(int)(left_side_width))],
+
+  # Web Addresses
+  [sg.Text("First page to scrape from")],
+  [sg.InputText(default_beginning_link, key='beginning_link', size=(left_side_width, 1))],
+  [sg.Text("Final page to scrape from (inclusive)")],
+  [sg.InputText(default_ending_link, key='ending_link', size=(left_side_width, 1))],
+  [sg.Text('_'*(int)(left_side_width))],
+
+  # Folder Chooser
+  [sg.Text("Please choose a destination folder")],
+  [sg.Text('Your Folder', size=(9, 1), auto_size_text=False, justification='left'),
+    sg.InputText(default_folder, size=(left_side_width-20, 1), key='folder_location'), sg.FolderBrowse()],
+
+  # Submit & Cancel Buttons
+  [sg.Submit(tooltip='Click to begin scraping'), sg.Button('Stop Program')],
+]
+
+
+# Console + Fun facts
+console_width = 80
+console_lines = 20
+right_side_layout = [
+  [sg.Output(size=(console_width, console_lines))],
+
+]
+
+
+layout = [
+  [sg.Frame(layout=left_side_layout, title='Options', relief=sg.RELIEF_SUNKEN),
+    sg.Frame(layout=right_side_layout, title='Console Log', relief=sg.RELIEF_SUNKEN)]
+]
+
+window_title = 'Wandering Inn Scraper'
+window = sg.Window(window_title, layout, default_element_size=(40, 1), grab_anywhere=False)
+
+confirm_title = "Confirm"
+
+
+# Event loop
+while(True):
+  event, values = window.read()
+
+  if event in (None, 'Exit'):
+    break
+
+
+  if debug: 
+    # print("Button:", event, " Values", values)
+    print("Button:", event)
+    print(getDebug(values))
+
+  if event == 'Submit':
+    # Check the data to make sure it's good, if not, throw error and continue loop
+
+
+    # Open up the confirm window
+    # confirm_window = sg.Window(confirm_title, confirm_layout, grab_anywhere=True)
+    confirmed = sg.PopupYesNo("Here's the info you input: ", getDebug(values),"Are you sure you want to submit?", title="Confirm", )
+    if confirmed == 'Yes':
+      print("Beginning Program")
+      start_url = values['beginning_link']
+      end_url = values['ending_link']
+      print_option = values['print_option']
+      directory = values['folder_location']
+      backend.scrapePageInit(start_url, end_url, print_option, directory)
+
+      print()
+      print('='*60)
+      print()
+      print("Congratulations! Your file(s) should be in the folder you specified")
+
+  if event == 'About...':
+    sg.popup(getAboutText(), title="About")
+
