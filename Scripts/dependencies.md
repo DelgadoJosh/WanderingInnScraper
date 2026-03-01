87234f6a5f301570b6e8f71e14270ef830c0e4e3
Josh Delgado <34148647+DelgadoJosh@users.noreply.github.com>
2026-03-01 02:54:52 -0800

    Initial port to tkinter

--- Scripts/dependencies.md
+++ Scripts/dependencies.md
@@ -3,14 +3,13 @@
 This python script has a few dependencies. 
 
 Dependencies:
-* PySimpleGUI - For the GUI
 * Requests - For the web requests
 * Beautiful Soup 4 - For parsing the html in Python 3
+* tkinter (Usually included with Python) - For the GUI
 
 ## How to Install dependencies
 Run the following commands:
 
-1. `pip install pysimplelgui`
 1. `pip install requests`
-1. `pip install beautifulsoup4`
+2. `pip install beautifulsoup4`
 
--- Scripts/wanderingInnScraperFrontEnd.py
+++ Scripts/wanderingInnScraperFrontEnd.py
@@ -1,151 +1,158 @@
-# This code creates a very simple GUI to run the Wandering Inn Scraper
-
-import PySimpleGUI as sg
+# This code creates a simple GUI to run the Wandering Inn Scraper using tkinter
+import sys
+import tkinter as tk
+from tkinter import ttk, filedialog, messagebox, scrolledtext
 import queue
 import threading 
 import wanderingInnScraperBackEnd as backend
 
-sg.theme('Dark Brown')
+class WanderingInnScraperGUI:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Wandering Inn Scraper")
+        self.root.geometry("1000x600")
+        
+        # Configure styles
+        style = ttk.Style()
+        style.theme_use('clam') # Using clam as it's cleaner than default on many systems
+        
+        # Variables
+        self.print_option = tk.StringVar(value='One Large File')
+        self.format_choice = tk.StringVar(value='txt')
+        self.beginning_link = tk.StringVar(value="https://wanderinginn.com/2017/03/03/rw1-00/")
+        self.ending_link = tk.StringVar(value="")
+        self.folder_location = tk.StringVar(value="")
+        
+        self.gui_queue = queue.Queue()
+        
+        self.setup_menu()
+        self.setup_layout()
+        
+        # Start queue polling
+        self.root.after(100, self.poll_queue)
 
+    def setup_menu(self):
+        menubar = tk.Menu(self.root)
+        
+        # File Menu
+        filemenu = tk.Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Exit", command=self.root.quit)
+        menubar.add_cascade(label="File", menu=filemenu)
+        
+        # Help Menu
+        helpmenu = tk.Menu(menubar, tearoff=0)
+        helpmenu.add_command(label="About...", command=self.show_about)
+        menubar.add_cascade(label="Help", menu=helpmenu)
+        
+        self.root.config(menu=menubar)
 
-# --- Helper Functions ---
-def getDebug(values):
-  textType = "Plain text" if(values['txt']) else "HTML"
+    def setup_layout(self):
+        # Main container with padding
+        main_frame = ttk.Frame(self.root, padding="10")
+        main_frame.pack(fill=tk.BOTH, expand=True)
+        
+        # Left Side - Options
+        options_frame = ttk.LabelFrame(main_frame, text="Options", padding="10")
+        options_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=False, padx=(0, 10))
+        
+        # Output type
+        ttk.Label(options_frame, text="Please select what type of output file:").pack(anchor=tk.W, pady=(0, 5))
+        options = ['One Large File', 'Individual Chapters', 'Both']
+        option_menu = ttk.OptionMenu(options_frame, self.print_option, options[0], *options)
+        option_menu.pack(fill=tk.X, pady=(0, 10))
+        
+        # Format choice
+        format_frame = ttk.Frame(options_frame)
+        format_frame.pack(fill=tk.X, pady=(0, 10))
+        ttk.Radiobutton(format_frame, text='Plain text', variable=self.format_choice, value='txt').pack(side=tk.LEFT, padx=(0, 10))
+        ttk.Radiobutton(format_frame, text='HTML', variable=self.format_choice, value='html').pack(side=tk.LEFT)
+        
+        ttk.Separator(options_frame, orient=tk.HORIZONTAL).pack(fill=tk.X, pady=10)
+        
+        # Links
+        ttk.Label(options_frame, text="First page to scrape from").pack(anchor=tk.W)
+        ttk.Entry(options_frame, textvariable=self.beginning_link, width=50).pack(fill=tk.X, pady=(0, 10))
+        
+        ttk.Label(options_frame, text="Final page to scrape from (inclusive)").pack(anchor=tk.W)
+        ttk.Entry(options_frame, textvariable=self.ending_link, width=50).pack(fill=tk.X, pady=(0, 10))
+        
+        ttk.Separator(options_frame, orient=tk.HORIZONTAL).pack(fill=tk.X, pady=10)
+        
+        # Folder Choice
+        ttk.Label(options_frame, text="Please choose a destination folder").pack(anchor=tk.W)
+        folder_frame = ttk.Frame(options_frame)
+        folder_frame.pack(fill=tk.X, pady=(0, 20))
+        ttk.Entry(folder_frame, textvariable=self.folder_location).pack(side=tk.LEFT, fill=tk.X, expand=True, padx=(0, 5))
+        ttk.Button(folder_frame, text="Browse", command=self.browse_folder).pack(side=tk.RIGHT)
+        
+        # Submit Button
+        ttk.Button(options_frame, text="Submit", command=self.on_submit).pack(pady=10)
+        
+        # Right Side - Console Log
+        console_frame = ttk.LabelFrame(main_frame, text="Console Log", padding="10")
+        console_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        
+        self.console = scrolledtext.ScrolledText(console_frame, state='disabled', wrap=tk.WORD, font=('TkFixedFont', 10))
+        self.console.pack(fill=tk.BOTH, expand=True)
 
-  text = ""
-  text += "Type of output file: " + values['print_option'] + ", " + textType + "\n"
-  text += "Beginning Link: " + values['beginning_link'] + "\n"
-  text += "Ending Link: " + values['ending_link'] + "\n"
-  text += "Folder Address: " + values['folder_location'] + "\n"
-  return text
+    def browse_folder(self):
+        folder = filedialog.askdirectory()
+        if folder:
+            self.folder_location.set(folder)
 
-def getAboutText():
-  text =  ""
-  text += "Wandering Inn Scraper\n"
-  text += "by Josh Delgado\n"
-  return text
+    def show_about(self):
+        messagebox.showinfo("About", "Wandering Inn Scraper\nby Josh Delgado")
 
-# Debug Setting
-debug = False
+    def get_debug_text(self):
+        text_type = "Plain text" if self.format_choice.get() == "txt" else "HTML"
+        text = f"Type of output file: {self.print_option.get()}, {text_type}\n"
+        text += f"Beginning Link: {self.beginning_link.get()}\n"
+        text += f"Ending Link: {self.ending_link.get()}\n"
+        text += f"Folder Address: {self.folder_location.get()}\n"
+        return text
 
+    def on_submit(self):
+        start_url = self.beginning_link.get().strip()
+        end_url = self.ending_link.get().strip()
+        print_option = self.print_option.get()
+        directory = self.folder_location.get().strip()
+        format_choice = self.format_choice.get()
 
-# --- Layout ---
-# Defines the Menu Header
-menu_def = [['File', ['Exit']],
-            ['Help', 'About...'],]
+        if not end_url:
+            messagebox.showwarning("Incomplete Information", "Make sure to add an ending url!")
+            return
 
-print_options = ('One Large File', 'Individual Chapters', 'Both',)
+        if not directory:
+            messagebox.showwarning("Incomplete Information", "Make sure to choose a folder!")
+            return
 
-# default_beginning_link = "https://wanderinginn.com/2016/07/27/1-00/"  # Pre re-write
-default_beginning_link = "https://wanderinginn.com/2017/03/03/rw1-00/" 
-default_ending_link = ""
+        confirm_msg = f"Here's the info you input:\n\n{self.get_debug_text()}\nAre you sure you want to submit?"
+        if messagebox.askyesno("Confirm", confirm_msg):
+            self.log("Beginning Program\n")
+            threading.Thread(target=backend.scrapePageInit,
+                            args=(start_url, end_url, print_option, directory, format_choice, self.gui_queue), 
+                            daemon=True).start()
 
-default_folder = ""
+    def log(self, text):
+        self.console.config(state='normal')
+        self.console.insert(tk.END, text + "\n")
+        self.console.config(state='disabled')
+        self.console.see(tk.END)
 
-# Options
-left_side_width = 50
-left_side_layout = [
-  # Menu
-  [sg.Menu(menu_def, tearoff=True)],
+    def poll_queue(self):
+        try:
+            while True:
+                message = self.gui_queue.get_nowait()
+                if message:
+                    self.log(message)
+                self.gui_queue.task_done()
+        except queue.Empty:
+            pass
+        finally:
+            self.root.after(100, self.poll_queue)
 
-  # Output Option
-  [sg.Text("Please select what type of output file:", size=(left_side_width, 1))],
-  [sg.InputOptionMenu(print_options, key='print_option', size=((int)(left_side_width*0.8), 1), default_value='One Large File')],
-  [sg.Radio('Plain text', "textRadio", key="txt", default=True, size=(10, 1)), sg.Radio('HTML', "textRadio", key="html")],
-  [sg.Text('_'*(int)(left_side_width))],
-
-  # Web Addresses
-  [sg.Text("First page to scrape from")],
-  [sg.InputText(default_beginning_link, key='beginning_link', size=(left_side_width, 1))],
-  [sg.Text("Final page to scrape from (inclusive)")],
-  [sg.InputText(default_ending_link, key='ending_link', size=(left_side_width, 1))],
-  [sg.Text('_'*(int)(left_side_width))],
-
-  # Folder Chooser
-  [sg.Text("Please choose a destination folder")],
-  [sg.Text('Your Folder', size=(9, 1), auto_size_text=False, justification='left'),
-    sg.InputText(default_folder, size=(left_side_width-20, 1), key='folder_location'), sg.FolderBrowse()],
-
-  # Submit & Cancel Buttons
-  [sg.Submit(tooltip='Click to begin scraping')],
-]
-
-
-# Console + Fun facts
-console_width = 80
-console_lines = 20
-right_side_layout = [
-  [sg.Output(size=(console_width, console_lines))],
-
-]
-
-
-layout = [
-  [sg.Frame(layout=left_side_layout, title='Options', relief=sg.RELIEF_SUNKEN),
-    sg.Frame(layout=right_side_layout, title='Console Log', relief=sg.RELIEF_SUNKEN)]
-]
-
-window_title = 'Wandering Inn Scraper'
-window = sg.Window(window_title, layout, default_element_size=(40, 1), grab_anywhere=False)
-
-confirm_title = "Confirm"
-
-
-# Queue used for communication between threads
-# Multi-threading used in order to prevent GUI-Freeze
-gui_queue = queue.Queue()
-
-
-# Event loop
-while(True):
-  event, values = window.read(timeout=100)   # Timeout used to ensure it's periodically checking the other thread
-
-  if event in (None, 'Exit'):
-    break
-
-
-  if debug: 
-    # print("Button:", event, " Values", values)
-    print("Button:", event)
-    print(getDebug(values))
-
-  if event == 'Submit':
-    # Check the data to make sure it's good, if not, throw error and continue loop
-    start_url = values['beginning_link']
-    end_url = values['ending_link']
-    print_option = values['print_option']
-    directory = values['folder_location']
-    format_choice = "txt" if(values['txt']) else "html"
-
-    if (end_url == ''):
-      sg.popup("Make sure to add an ending url!")
-      continue
-
-    if (directory == ''):
-      sg.popup("Make sure to choose a folder!")
-      continue
-
-    # At this point, the data should be verified
-    # Open up the confirm window
-    # confirm_window = sg.Window(confirm_title, confirm_layout, grab_anywhere=True)
-    confirmed = sg.PopupYesNo("Here's the info you input: ", getDebug(values),"Are you sure you want to submit?", title="Confirm", )
-    if confirmed == 'Yes':
-      print("Beginning Program")
- 
-      threading.Thread(target=backend.scrapePageInit,
-                        args=(start_url, end_url, print_option, directory, format_choice, gui_queue), daemon=True).start()
-
-  # If the scraper back-end attempted to print information, 
-  # print that information
-  try: 
-    message = gui_queue.get_nowait() 
-  except queue.Empty:
-    message = None 
-
-  if message: 
-    print(message)
-
-
-  if event == 'About...':
-    sg.popup(getAboutText(), title="About")
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = WanderingInnScraperGUI(root)
+    root.mainloop()
 
